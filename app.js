eb2f976df0a45cc3d48f58b8be3477f23874301f
Shashank Gupta <shashankcm90@gmail.com>
2019-06-30 01:35:22 -0400

    created an graphql mutation for creating and event and a query to return list of events, but we here using in memory to store all events

--- app.js
+++ app.js
@@ -1,13 +1,64 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const graphqlHttp = require("express-graphql");
+const { buildSchema } = require("graphql");
 
 const app = express();
 
-app.use(bodyParser.json());
+const events = [];
 
-app.get("/", (req, res, next) => {
-  res.send("Testing in port 4000");
-});
+app.use(bodyParser.json());
+app.use(
+  "/graphql",
+  graphqlHttp({
+    schema: buildSchema(`
+
+      type Event {
+        _id: ID!
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      input EventInput {
+        title: String!
+        description: String!
+        price: Float!
+        date: String!
+      }
+
+      type RootQuery{
+        events: [Event!]!
+      }
+
+      type RootMutation {
+        createEvent(eventInput: EventInput): Event
+      }
+      schema {
+        query: RootQuery
+        mutation: RootMutation
+      }
+    `),
+    rootValue: {
+      events: () => {
+        return events;
+      },
+      createEvent: args => {
+        const event = {
+          _id: Math.random().toString(),
+          title: args.eventInput.title,
+          description: args.eventInput.description,
+          price: +args.eventInput.price,
+          date: args.eventInput.date
+        };
+        events.push(event);
+        return event;
+      }
+    },
+    graphiql: true
+  })
+);
 
 app.listen(4000, () => {
   console.log("App listening on port 4000!");
