7b7639b160f86e9443fa8612f4699ec6b5369869
Shashank Gupta <shashankcm90@gmail.com>
2019-06-30 02:07:33 -0400

    Implemented local  MongoDB database connection logic

--- app.js
+++ app.js
@@ -2,43 +2,46 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const graphqlHttp = require("express-graphql");
 const { buildSchema } = require("graphql");
+const mongoose = require("mongoose");
 
+const config = require("./DB");
 const app = express();
 
-const events = [];
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+const events = [];
 app.use(
   "/graphql",
   graphqlHttp({
     schema: buildSchema(`
-
-      type Event {
-        _id: ID!
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      input EventInput {
-        title: String!
-        description: String!
-        price: Float!
-        date: String!
-      }
-
-      type RootQuery{
-        events: [Event!]!
-      }
-
-      type RootMutation {
-        createEvent(eventInput: EventInput): Event
-      }
-      schema {
-        query: RootQuery
-        mutation: RootMutation
-      }
+    
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+    
+    type RootQuery{
+      events: [Event!]!
+    }
+    
+    type RootMutation {
+      createEvent(eventInput: EventInput): Event
+    }
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
     `),
     rootValue: {
       events: () => {
@@ -59,7 +62,15 @@ app.use(
     graphiql: true
   })
 );
-
-app.listen(4000, () => {
-  console.log("App listening on port 4000!");
-});
+mongoose.Promise = global.Promise;
+mongoose
+  .connect(config.DB, { useNewUrlParser: true })
+  .then(() => {
+    console.log("Database is connected");
+    app.listen(4000, () => {
+      console.log("App listening on port 4000!");
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
